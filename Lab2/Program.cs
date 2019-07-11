3c242281569daf08f03efff0ea06042ae354f57d
maniknath <maniknath79@gmail.co>
2019-07-10 21:26:51 -0400

    Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l = 0;
+            double w;
+            double h;
+            double param;
+            bool cont = true;
+
+            Console.WriteLine("Welcome to Grand Circus’ Room Detail Generator!");
+
+            while (cont == true)
+            {
+
+                Console.WriteLine("Enter Length:");
+                l = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Weidth:");
+                w = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Area:" + l * w);
+                param = 2 * l + 2 * w;
+                Console.WriteLine("Perimeter:" + param);
+
+
+                Console.WriteLine("Continue? Y/N");
+                string answer = Console.ReadLine();
+
+                if (answer == "Y" || answer == "y")
+                {
+                    cont = true;
+                }
+                else
+
+                {
+                    cont = false;
+
+                    Console.WriteLine("Thanks");
+                }
+
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+}
